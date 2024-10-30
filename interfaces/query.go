6b5693e882db2d9dca06ad81bb110b252eb4eb13
Fred Offei <112915741+danexpress@users.noreply.github.com>
2024-10-30 02:12:42 -0400

    boilerplate

---
+++ interfaces/query.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type dollars float32
+
+func (d dollars) String() string {
+	return fmt.Sprintf("$%.2f", d)
+
+}
+
+type database map[string]dollars
+
+func main() {
+	db := database{
+		"shoe":  50,
+		"socks": 5,
+	}
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
