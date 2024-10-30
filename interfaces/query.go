c460f82350f6654b3a5dd247d363235969fc06fc
Fred Offei <112915741+danexpress@users.noreply.github.com>
2024-10-30 02:59:08 -0400

    added add

--- interfaces/query.go
+++ interfaces/query.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
 )
 
 type dollars float32
@@ -15,11 +16,43 @@ func (d dollars) String() string {
 
 type database map[string]dollars
 
+func (db database) list(w http.ResponseWriter, req *http.Request) {
+	for item, price := range db {
+		fmt.Fprint(w, "%s: %s\n", item, price)
+	}
+}
+
+func (db database) add(w http.ResponseWriter, req *http.Request) {
+	item := req.URL.Query().Get("item")
+	price := req.URL.Query().Get("price")
+
+	if _, ok := db[item]; ok {
+		msg := fmt.Sprintf("duplicate item: %q", item)
+		http.Error(w, msg, http.StatusBadRequest)
+		return
+	}
+
+	p, err := strconv.ParseFloat(price, 32)
+
+	if err != nil {
+		msg := fmt.Sprintf("invalid price: %q", price)
+		http.Error(w, msg, http.StatusBadRequest)
+		return
+	}
+
+	db[item] = dollars(p)
+
+	fmt.Fprintf(w, "added %s with price %s\n", item, db[item])
+}
+
 func main() {
 	db := database{
 		"shoe":  50,
 		"socks": 5,
 	}
+
+	http.HandleFunc("/list", db.list)
+	http.HandleFunc("/create", db.add)
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
